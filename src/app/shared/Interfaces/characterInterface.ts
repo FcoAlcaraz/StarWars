2027c1fc7886eb16e16e527ccc5ccf1469671d61
Alcaraz, Francisco <francisco.alcaraz@poly.com>
2023-03-31 18:24:42 -0700

    Interfaces

---
+++ src/app/shared/Interfaces/characterInterface.ts
@@ -0,0 +1,17 @@
+export interface Character {
+  id: number;
+  name: string;
+  birth_year: string;
+  eye_color: string;
+  films: [];
+  gender: string;
+  created: string;
+  status: string;
+  skin_color: string;
+  hair_color: string;
+  height: string;
+  homeworld: string;
+  mass: string;
+  edited: string;
+  species: [];
+}
