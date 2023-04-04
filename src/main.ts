3853734a6f4532889ccc49414bd7b788fb718e3f
FcoAlcaraz <84114614+FcoAlcaraz@users.noreply.github.com>
2023-04-04 11:29:45 -0700

    1 file modified, 1 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
-import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { HomeComponent } from '@app/components/home/home.component';
+// import 'zone.js/dist/zone';
+// import { Component } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { HomeComponent } from './home/home.component';
+// import { AppComponent } from './app/app.component';
 
-@Component({
-  selector: 'my-app',
-  standalone: true,
-  imports: [CommonModule, HomeComponent, AppComponent],
-  templateUrl: `./app/app.component.html`,
-})
-export class App {
-  name = 'Angular';
-}
+// @Component({
+//   selector: 'my-app',
+//   standalone: true,
+//   imports: [CommonModule, HomeComponent, AppComponent],
+//   templateUrl: `./app/app.component.html`,
+// })
+// export class App {
+//   name = 'Angular';
+// }
 
-bootstrapApplication(App);
+// bootstrapApplication(App);
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
